e37060317a126653fa8a9f65043702b8e54f8667
ArnabDey1543 <arnabdey1543@gmail.com>
2023-03-20 23:16:49 +0530

    adding files

---
+++ README.md
@@ -0,0 +1,16 @@
+# MoreillonPay
+
+Note: This application has been completely refactored because it was originally written at a time when I knew what I was doing even less than today.
+
+Refactoring efforts invovled:
+
+- Using Mongoose for the consistency of records schema
+- Routing respecting OpenAPI standards
+- Async await
+- General code readability
+
+I did not have time to fully test this refactored version so please excuse any bug.
+
+Please find the source code for the GUI of this project [here](https://github.com/maximemoreillon/moreillon_pay_front) and the IoT reader firmware [here](https://github.com/maximemoreillon/moreillon_pay_reader).
+
+For more information about this project, please see [this article](https://articles.maximemoreillon.com/articles/133).
---
+++ controllers/transactions.js
@@ -0,0 +1,73 @@
+const Transaction = require('../models/transaction')
+const User = require('../models/user')
+
+const createHttpError = require('http-errors')
+
+exports.create_transaction = async (req, res, next) => {
+
+  const { user: currentUser } = res.locals
+
+  const {
+    card_uuid,
+    user_id,
+    amount,
+    description
+  } = req.body
+
+  try {
+    // TODO: consider case where top up from admin
+
+    const query = (currentUser && currentUser.isAdmin) 
+      ? {$or: [{card_uuid},{_id: user_id}]} 
+      : {card_uuid}
+
+    const user = await User.findOne(query)
+
+    if(!user) throw createHttpError(404, `User with card ${card_uuid} not found`)
+    if(user.balance < Number(amount)) throw createHttpError(400, `Insufficient balance`)
+
+    user.balance += Number(amount)
+
+    await user.save()
+
+    const newTransactionProperties = {
+      user_id: user._id,
+      amount,
+      description,
+    }
+
+    const newTransaction = await Transaction.create(newTransactionProperties)
+    console.log(`Transaction ${newTransaction._id} registered`)
+    
+    // TODO: Improve require
+    require('../index.js').io.sockets.emit('user_updated', user)
+
+    res.send(newTransaction)
+
+
+  } catch (error) {
+    next(error)
+  }
+
+}
+
+
+exports.read_transactions = async (req, res, next) => {
+  try {
+
+    // TODO: apply filters
+    const {
+      from,
+      to,
+      ...query
+    } = req.query
+
+    const transactions = await Transaction.find({})
+
+    console.log(`Transactions queried`)
+    res.send(transactions)
+
+  } catch (error) {
+    next(error)
+  }
+}
